1907168ce9507f7463fe095cce4fb22d8209025e
SabariVarshan07 <sabarivarshan47@gmail.com>
2026-09-03 11:05:39 +0530

    Modify Course table column data types
    Updated data types for Course table columns.

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -3,10 +3,10 @@ CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
 CREATE TABLE Course (
-    CourseID NUMBER(5) PRIMARY KEY,
-    CourseName VARCHAR2(30),
-    Credits NUMBER(2),
-    DepartmentID NUMBER(5)
+    CourseID int(5) PRIMARY KEY,
+    CourseName VARCHAR(30),
+    Credits int(2),
+    DepartmentID int (5)
 );
 INSERT INTO Course VALUES (201, 'DBMS', 4, 101);
 INSERT INTO Course VALUES (202, 'Java', 3, 102);
